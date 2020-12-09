14c6145b9b112dbed79144f23739bc4b9979fd89
Julia Fischer <julia.fischer@campudus.com>
2020-12-09 16:07:18 +0100

    finished welcome page with logo

--- app/src/main/java/com/example/shopping_cart/MainActivity.java
+++ app/src/main/java/com/example/shopping_cart/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.shopping_cart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int WELCOME_TIME_OUT = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, WELCOME_TIME_OUT);
     }
 }
